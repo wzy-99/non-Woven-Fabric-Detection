f471b0982e3e7418b0e08e1611fe705b05956cc3
ZCJ <lhwzy99@163.com>
2020-12-02 15:15:04 +0800

    v2 release

--- Detector.cpp
+++ Detector.cpp
@@ -1,4 +1,5 @@
 #include "Detector.h"
+#include <algorithm>
 #include <opencv2/imgproc.hpp>
 
 Detector::Detector()
@@ -21,14 +22,15 @@ void Detector::set(Param& param)
 	this->param = param;
 }
 
-void Detector::detect(cv::Mat& img)
+std::vector<Detecion>& Detector::detect(cv::Mat& img)
 {
 	this->img = img;
 
 	segmenting();
 	searching();
-	growing();
 	sorting();
+
+	return this->detection;
 }
 
 void Detector::segmenting()
@@ -47,43 +49,127 @@ void Detector::segmenting()
 
 void Detector::searching()
 {
+	uint16_t label = 0;
 	for (uint16_t i = 0; i < this->binary.rows; i++)
 	{
 		for (uint16_t j = 0; j < this->binary.cols; j++)
 		{
 			if (this->binary.at<uchar>(i, j) == DIRTY)
 			{
-				this->seed.push_back(Point(i, j));
+				growing(i, j, label);
 			}
 		}
 	}
 }
 
-void Detector::growing()
+void Detector::growing(uint16_t y, uint16_t x, uint16_t label)
 {
-	uint16_t label = 1;
-
 	uint16_t max_y = this->binary.rows - 1;
 	uint16_t max_x = this->binary.cols - 1;
 
-	for (uint16_t i = 0; i < this->seed.size(); i++)
+	std::stack<Point> points;
+	points.push(Point(y, x));
+
+	while (!points.empty())
 	{
-		Point point = this->seed.at(i);
+		// 先拷贝然后POP
+		Point point(points.top());
+		points.pop();
 
-		if (point.label == 0)  // unlabeled
+		// 将该点设为已访问过
+		this->binary.at<uchar>(point.y, point.x) = VISIT;
+
+		// 开辟空间，并记录该点
+		while (this->detection.size() < label)
 		{
-			point.label = label;
-			label = label + 1;
+			this->detection.push_back(Detecion());
 		}
+		this->detection[label].area.push_back(point);
 
-		if (point.x < max_x)
+		// 判断
+		if (point.x > 0) //left
 		{
-			if (this->binary.at<uchar>(point.x + 1, point.y) == DIRTY)  // right
+			if (this->binary.at<uchar>(point.y, point.x - 1) == DIRTY)
 			{
-				this->seed.push_back(Point(i, j));
+				points.push(Point(y, x - 1));
+			}
+		}
+		if (point.x < max_x) //right
+		{
+			if (this->binary.at<uchar>(point.y, point.x + 1) == DIRTY)
+			{
+				points.push(Point(y, x + 1));
+			}
+		}
+		if (point.y > 0) //up
+		{
+			if (this->binary.at<uchar>(point.y - 1, point.x) == DIRTY)
+			{
+				points.push(Point(y - 1, x));
+			}
+		}
+		if (point.y < 0) //down
+		{
+			if (this->binary.at<uchar>(point.y + 1, point.x) == DIRTY)
+			{
+				points.push(Point(y + 1, x));
 			}
 		}
 	}
 }
 
+void Detector::sorting()
+{
+	for (std::vector<Detecion>::iterator iter = this->detection.begin(); iter != this->detection.end(); iter++)
+	{
+		if ((*iter).area.empty())
+		{
+			this->detection.erase(iter);
+		}
+		else if((*iter).area.size() < this->param.dirty_block_size)
+		{
+			this->detection.erase(iter);
+		}
+		else
+		{
+			(*iter).measure = (*iter).area.size();
 
+			uint16_t min_x = this->binary.rows;
+			uint16_t min_y = this->binary.cols;
+			uint16_t max_x = 0;
+			uint16_t max_y = 0;
+			for (std::vector<Point>::iterator it = (*iter).area.begin(); it != (*iter).area.end(); iter++)
+			{
+				if ((*it).x < min_x)
+				{
+					min_x = (*it).x;
+				}
+				if ((*it).x > max_x)
+				{
+					max_x = (*it).x;
+				}
+				if ((*it).y < min_y)
+				{
+					min_y = (*it).y;
+				}
+				if ((*it).y > max_y)
+				{
+					max_y = (*it).y;
+				}
+			}
+
+			(*iter).width = max_x - min_x > 1 ? max_x - min_x : 1;			// 宽度
+			(*iter).height = max_y - min_y > 1 ? max_y - min_y : 1;			// 高度
+			(*iter).aspect = (double)(*iter).width / (double)(*iter).height;// 长宽比
+
+			if ((*iter).aspect > this->param.dirty_aspect_thres || 1 / (*iter).aspect > this->param.dirty_aspect_thres)
+			{
+				(*iter).type = HAIR;
+			}
+			else
+			{
+				(*iter).type = DUST;
+			}
+		}
+	}
+}
--- Detector.h
+++ Detector.h
@@ -1,40 +1,80 @@
 #pragma once
+
+/**
+	无纺布检测
+**/
+
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <opencv2/core.hpp>
 
 #define CLEAN	255
 #define DIRTY	0
+#define VISIT	127
 
+#define DUST	1
+#define HAIR	2
+
+
+/**
+	@class:		算法参数
+	@describe:	具体详见注释。
+**/
 struct Param
 {
 	uint16_t segment_block_size;
 	uint16_t segment_constant;
+	uint16_t dirty_block_size;
+	double dirty_aspect_thres;
+
+	void operator=(const Param& param)
+	{
+		this->segment_block_size = param.segment_block_size;
+		this->segment_constant = param.segment_constant;
+		this->dirty_block_size = param.dirty_block_size;
+		this->dirty_aspect_thres = param.dirty_aspect_thres;
+	}
 };
 
+/**
+	@class:		检测结果
+	@describe:	包括类型、面积、长宽比、区域。
+**/
 struct Detecion
 {
+	Detecion()
+	{
+		this->type = 0;
+		this->measure = 0;
+		this->aspect = 0.0;
+		this->width = 0;
+		this->height = 0;
+	}
 	uint16_t type;								// 类型
 	uint16_t measure;							// 面积
+	uint16_t width;								// 宽度
+	uint16_t height;							// 长度
 	double aspect;								// 长宽比
-	std::vector<std::vector<cv::Point>> area;	// 区域
+	std::vector<Point> area;					// 连通域
 };
 
+/**
+	@class:		二维点
+	@describe:	记录Y和X坐标。
+**/
 struct Point
 {
-	Point(uint16_t x, uint16_t y)
+	Point(Point& point)
 	{
-		this->label = 0;
+		this->x = point.x;
+		this->y = point.y;
+	};
+	Point(uint16_t y, uint16_t x)
+	{
 		this->x = x;
 		this->y = y;
 	};
-	Point(uint16_t label, uint16_t x, uint16_t y)
-	{
-		this->label = label;
-		this->x = x;
-		this->y = y;
-	};
-	uint16_t label;
 	uint16_t x;
 	uint16_t y;
 };
@@ -43,7 +83,6 @@ struct Point
 	@class:		检测类
 	@describe:	将检测算法封装，使其与环境无关。
 **/
-
 class Detector
 {
 public:
@@ -54,7 +93,7 @@ public:
 
 	// interface fuction
 	void set(Param& param);
-	void detect(cv::Mat& img);
+	std::vector<Detecion>& detect(cv::Mat& img);
 
 private:
 	// config variable
@@ -63,14 +102,13 @@ private:
 	// local variable
 	cv::Mat img;
 	cv::Mat binary;
-	std::vector<Point> seed;
 	std::vector<Detecion> detection;
 
 	// inner fuction
-	void segmenting();		// 分割图像
-	void searching();		// 搜索种子点
-	void growing();			// 区域生长
-	void sorting();			// 结果分类
+	void segmenting();									// 分割图像
+	void searching();									// 搜索种子点
+	void growing(uint16_t, uint16_t, uint16_t);			// 区域生长
+	void sorting();										// 结果分类
 };
 
 
